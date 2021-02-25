3da308dc49818d47ebc0564175a4ce2be17e9b98
Richard Carvalho <richardcarvalhooo@gmail.com>
2021-02-25 17:54:09 -0300

    feat: Coding controller

---
+++ src/controllers/Starship.js
@@ -0,0 +1,112 @@
+import { useState } from 'react';
+import api from '../services/api';
+
+export const StarshipController = () => {
+  const [loading, setLoading] = useState(false);
+  const [starships, setStarships] = useState([]);
+  const [inputText, setInputText] = useState('');
+
+  const handleInputText = (text) => {
+    if (/^\d+$/.test(text)) {
+      setInputText(text);
+    }
+  };
+
+  const calculator = {
+    day: (megalights, consumables, timeUnityData) => {
+      return parseInt(
+        Number(megalights) / Number(consumables) / 24 / Number(timeUnityData),
+        0,
+      );
+    },
+    days: (megalights, consumables, timeUnityData) => {
+      return parseInt(
+        Number(megalights) / Number(consumables) / 24 / Number(timeUnityData),
+        0,
+      );
+    },
+    week: (megalights, consumables, timeUnityData) => {
+      return parseInt(
+        Number(megalights) /
+          Number(consumables) /
+          24 /
+          7 /
+          Number(timeUnityData),
+        0,
+      );
+    },
+    weeks: (megalights, consumables, timeUnityData) => {
+      return parseInt(
+        Number(megalights) /
+          Number(consumables) /
+          24 /
+          7 /
+          Number(timeUnityData),
+        0,
+      );
+    },
+    month: (megalights, consumables, timeUnityData) => {
+      return parseInt(
+        Number(megalights) /
+          Number(consumables) /
+          24 /
+          30 /
+          Number(timeUnityData),
+        0,
+      );
+    },
+    months: (megalights, consumables, timeUnityData) => {
+      return parseInt(
+        Number(megalights) /
+          Number(consumables) /
+          24 /
+          30 /
+          Number(timeUnityData),
+        0,
+      );
+    },
+    year: (megalights, consumables, timeUnityData) => {
+      return parseInt(
+        Number(megalights) /
+          Number(consumables) /
+          24 /
+          365 /
+          Number(timeUnityData),
+        0,
+      );
+    },
+    years: (megalights, consumables, timeUnityData) => {
+      return parseInt(
+        Number(megalights) /
+          Number(consumables) /
+          24 /
+          365 /
+          Number(timeUnityData),
+        0,
+      );
+    },
+  };
+
+  const calculateStops = async (megalights) => {
+    setLoading(true);
+    const { data } = await api.get('/starships');
+    for (const starship of data.results) {
+      const words = starship.consumables.split(' ');
+      starship.stops = calculator[words[1]](
+        megalights,
+        words[0],
+        starship.MGLT,
+      );
+    }
+    setStarships(data.results);
+    setLoading(false);
+  };
+
+  return {
+    starships,
+    loading,
+    calculateStops,
+    inputText,
+    handleInputText,
+  };
+};
